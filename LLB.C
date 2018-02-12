8720c86e914c69cf87285f69ac7af17099cbd266
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-12 13:11:34 +0530

    Create LLB.C

---
+++ LLB.C
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int x[100],n,i;
+float mean(int,int[]);
+float median(int,int[]);
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+    scanf("%d",&x[i]);
+printf("mean=%f\n",mean(n,x));
+printf("median=%f\n",median(n,x));
+getch();
+}
+
+float mean(int m, int a[]) {
+    int sum=0, i;
+    for(i=0; i<m; i++)
+        sum+=a[i];
+    return((float)sum/m);
+}
