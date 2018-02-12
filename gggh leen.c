4ca54a4de33d7fe32ba6de3e99b6802b5c9c394a
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-12 14:11:04 +0530

    Update gggh leen.c

--- gggh leen.c
+++ gggh leen.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
 	int a,b,temp;
 	printf("enter a and b\n");
 	scanf("%d\n%d",&a,&b);
 	temp=a;
-  
 	a=b;
 	b=temp;
 	printf("%d\n%d",a,b);
 	return 0;
+	getch ();
 }
